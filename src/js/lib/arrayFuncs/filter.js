b8d5a4efd718aadef3ee8d61ea9ace6c9dbf9aa0
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-04 14:16:51 +0200

    🚧 added one filter sync result example

--- src/js/lib/arrayFuncs/filter.js
+++ src/js/lib/arrayFuncs/filter.js
@@ -2,7 +2,11 @@ const personsSync = require('../../dataset/persons.Sync');
 const personsASync = require('../../dataset/persons.Async');
 
 
-const filterResults = 'filterResults'
+const filterResults = {
+    'person\'s names named Rick': personsSync
+                                        .filter(person => person.name.includes('Rick'))
+                                        .map(person => person.name)
+}
 // const filterResults = persons.filter(person => person.status === 'Alive')
 
 module.exports = filterResults;
